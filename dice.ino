a53ce14dc6df5b2f683616d0dafe407abae92438
Matthew Grogan <mvgnyc@gmail.com>
2017-03-05 11:29:41 -0500

    Working version

--- dice.ino
+++ dice.ino
@@ -2,19 +2,14 @@
  * This code is to build a single die with two tilt switches, six LEDs, and an ATTINY45.
  */
 
-// Orientation constants
-const int UP = 1;
-const int DOWN = 2;
-
-// Tilt sensor pins
-const int UP_PIN = 0;
-const int DOWN_PIN = 1;
+// Tilt sensor pin
+#define UP_PIN 6
 
 // Charliplexed LED Pins
-#define P1 2
-#define P2 3
-#define P3 4
-#define P4 5
+#define P1 5
+#define P2 2
+#define P3 3
+#define P4 4
 
 const int pins[4] = {P1, P2, P3, P4};
 
@@ -26,10 +21,6 @@ const int pins[4] = {P1, P2, P3, P4};
 #define DELAY 1
 #define CYCLES 1000
 
-// Delay between number display
-#define PAUSE 100
-
-
 // Which pips are lit for each die roll 1..6?
 /*
  *    --------- 
@@ -40,26 +31,37 @@ const int pins[4] = {P1, P2, P3, P4};
  * 
  */
 const byte PIPS[] = {
-  0b01000000,
-  0b00010010,
-  0b01100001,
-  0b01001101,
-  0b01101101,
-  0b00111111,
-  0b01111111,
+  0b01000000, // one
+  0b00010010, // two
+  0b01100001, // three
+  0b00101101, // four
+  0b01101101, // five
+  0b00111111, // six
+  0b01111111, // seven
 };
 
 #define OFF -1
 const int pin_map[][4] = {
-  {OFF, OFF, OFF, OFF},
-  {LOW, HIGH, OFF, OFF},
-  {HIGH, LOW, OFF, OFF},
-  {OFF, OFF, HIGH, LOW},
-  {OFF, OFF, LOW, HIGH},
-  {OFF, LOW, HIGH, OFF},
-  {OFF, HIGH, LOW, OFF},
-  {HIGH, OFF, LOW, OFF}
+  {OFF, OFF, OFF, OFF},  // zero
+  {OFF, OFF, LOW, HIGH}, // one
+  {OFF, LOW, OFF, HIGH}, // two
+  {LOW, OFF, OFF, HIGH}, // three
+  {OFF, OFF, HIGH, LOW}, // four
+  {OFF, HIGH, OFF, LOW}, // five
+  {HIGH, OFF, OFF, LOW}, // six
+  {OFF, LOW, HIGH, OFF}  // seven
 };
+
+void clear_pins() {
+  for (int pid = 0; pid < 4; pid++) {
+    
+     // Look up the pin number in the array
+     int pin = pins[pid];
+
+     // Set all pins as input
+      pinMode(pin, INPUT);
+  }
+}
 
 void light_pip(int pip) {
   // Light a single pip on the die
@@ -88,8 +90,9 @@ void light_pip(int pip) {
   }
 }
 
+/*
 void clear_die() {
-    // Turn off all pips
+  // Turn off all pips
   for (int pip = 0; pip < NUMPIPS; pip++) {
     for (int pid = 0; pid < 4; pid++) {
   
@@ -101,6 +104,7 @@ void clear_die() {
     }
   }
 }
+*/
 
 void light_die(int val, int cycles=CYCLES) {
   // Light multiple pips for a die value
@@ -112,73 +116,77 @@ void light_die(int val, int cycles=CYCLES) {
 
       // Shift the bit mask
       int mask = 1 << pip;
+
       
       if ((pip_map & mask) == mask) {
         light_pip(pip + 1);
+        //Serial.println("Lighting pip+1");
       }
       delay(DELAY);
     }
     
   }
 
-  clear_die();
+  clear_pins();
 }
 
 int old_state = 0;
 
-int read_orientation() {
-  // Read the tilt sensor value and determine the orientation
-  int up = digitalRead(UP_PIN) << 1; // Bitshift by one
-  int down = digitalRead(DOWN_PIN);
-
-  return up + down;
-}
-
 void setup() {
   //start serial connection
-  //Serial.begin(9600);
-  randomSeed(analogRead(0));
+  Serial.begin(9600);
+  //randomSeed(analogRead(0));
+
+  // Light each pip in sequence
+  for (int p = 1; p <= 7; p++) {
+    light_pip(p);
+    delay(250); 
+  }
+
+  // Light each die value in sequence
+  for (int d = 1; d <= 6; d++) {
+    clear_pins();
+    delay(100);
+    light_die(d, 100);
+  }
 
   // Blink all LEDs three times
   for (int i=0; i < 3; i++) {
-    light_die(7, 50);
-    delay(75);
+    clear_pins();
+    delay(100);
+    light_die(7, 100);
+    delay(100);
   }
 
   // Configure the two pins
-  pinMode(UP_PIN, INPUT_PULLUP);
-  pinMode(DOWN_PIN, INPUT_PULLUP);
+  pinMode(UP_PIN, INPUT);
 
   // Add interrupts to call any time the pin values change
   // Using interrupts to help save some power
-  attachInterrupt(digitalPinToInterrupt(UP_PIN), check_state, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(UP_PIN), check_state, CHANGE);  
+  //attachInterrupt(digitalPinToInterrupt(UP_PIN), check_state, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(DOWN_PIN), check_state, CHANGE);  
 }
 
 void check_state() {
   
   // Obtain the new state from the pins
-  int new_state = read_orientation();
+  int new_state = digitalRead(UP_PIN);
 
   if (new_state != old_state) {
-    // Only output if the state has changed
-    switch(new_state) {
-      case UP:
-        light_die(random(1, 7));
-        break;
-       case DOWN:
-        //Serial.println("DOWN");
-        break;
-       default:
-        //Serial.println("SIDEWAYS");
-        break;
+
+    if (new_state) {
+      light_die(random(1, 7));
     }
+
     old_state = new_state;
   }
 }
 
 void loop() {
-  // Nothing to do
+  //light_die(random(1, 7));
+  check_state();
+  delay(100);
+  
 }
 
 
